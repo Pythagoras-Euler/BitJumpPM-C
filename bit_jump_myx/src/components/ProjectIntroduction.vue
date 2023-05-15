35c3234c2a9f7d7db9f546712b2f00db591e6319
AnsrM <75668184+AnsrM@users.noreply.github.com>
2023-05-15 15:34:16 +0800

    project created
    Part3 project created by 孟宇鑫

---
+++ bit_jump_myx/src/components/ProjectIntroduction.vue
@@ -0,0 +1,29 @@
+<script setup>
+  import {reactive} from "vue";
+
+  export default {
+      name:'ProjectIntroduction',
+      setup() {
+          let data = reactive({
+              projectUrl:'http://http://rjpy.iq/ikuejpjy',
+              projectName:'123',
+              leaderName:'myx',
+              budget:'1000',
+              introduction:'dolor nulla do exercitation nostrud',
+          })
+
+          return {
+              data
+          }
+      }
+  }
+</script>
+
+<template>
+  <h2>项目名：</h2>
+  <
+</template>
+
+<style scoped>
+
+</style>
