7bdf0a3923e55c09b05f5e8ad1b4008a4eae3425
pfcs33 <aaapfcs@gmail.com>
2023-06-03 11:06:07 +0800

    完成参与项目的项目详情界面，责任人必须是自己才显示提交按钮的逻辑判断

--- FirstPart_Vite/src/components/project/projectDetail/ProjectProcess.vue
+++ FirstPart_Vite/src/components/project/projectDetail/ProjectProcess.vue
@@ -1,5 +1,6 @@
 <script>
 import { computed, reactive } from "vue";
+import { useStore } from "vuex";
 import PopupAddTask from "./PopupAddTask.vue";
 import PopupReject from "./PopupReject.vue";
 import PopupSubmit from "./PopupSubmit.vue";
@@ -19,6 +20,9 @@ export default {
   },
 
   setup(props) {
+    const store = useStore();
+    const userId = store.getters["userId"];
+
     let data = reactive({
       sortOrder: 0,
       isPopupRejectOpen: false,
@@ -88,6 +92,7 @@ export default {
       reject,
       submit,
       addTask,
+      userId,
     };
   },
 };
@@ -138,9 +143,9 @@ export default {
           >
             驳回
           </button>
-          <!--                    todo 这里还应该有个判断条件：责任人必须是自己才显示提交按钮-->
+          <!--                    todo 这里还应该有个判断条件：责任人必须是自己才显示提交按钮(已完成)-->
           <button
-            v-if="!isManager && data.isFinish"
+            v-if="!isManager && data.isFinish && data.ownerId === userId"
             class="submit-button"
             @click="submit(data)"
           >
